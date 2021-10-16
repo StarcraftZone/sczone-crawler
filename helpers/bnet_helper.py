1419622cc9f83d3a0edd6abd048de6efc89c9878
Bean <bean@yuanfen.net>
2021-10-16 09:55:49 +0800

    完成遍历 character 的功能；并添加索引

--- helpers/bnet_helper.py
+++ helpers/bnet_helper.py
@@ -1,22 +1,112 @@
 import requests
+import json
+from datetime import datetime
 from .config_helper import config
 from .redis_helper import redis_connection
 
 
 def get_access_token():
     access_token = redis_connection.get("sczone-crawler:token:battlenet")
-    if access_token is not None:
-        print("cached access token: " + access_token)
-        return access_token
+    if access_token is None:
+        bnetClientId = config["credentials"]["bnetClientId"]
+        bnetClientSecret = config["credentials"]["bnetClientSecret"]
+        response = requests.post(
+            "https://www.battlenet.com.cn/oauth/token",
+            auth=(bnetClientId, bnetClientSecret),
+            data={"grant_type": "client_credentials"},
+        )
+        response_data = response.json()
+        access_token = response_data["access_token"]
+        expires_in = response_data["expires_in"]
+        redis_connection.set("sczone-crawler:token:battlenet", access_token, expires_in)
+        print("fresh access token: " + access_token)
+    return access_token
 
-    bnetClientId = config["credentials"]["bnetClientId"]
-    bnetClientSecret = config["credentials"]["bnetClientSecret"]
-    response = requests.post(
-        "https://www.battlenet.com.cn/oauth/token",
-        auth=(bnetClientId, bnetClientSecret),
-        data={"grant_type": "client_credentials"},
-    )
+
+def get_api_response(path):
+    url = f"https://gateway.battlenet.com.cn{path}?locale=en_US&access_token={get_access_token()}"
+    response = requests.get(url)
     response_data = response.json()
-    redis_connection.set("sczone-crawler:token:battlenet", response_data["access_token"], response_data["expires_in"])
-    print("fresh access token: " + access_token)
-    return response_data["access_token"]
+    return response_data
+
+
+def get_league(localized_game_mode):
+    parts = localized_game_mode.split(" ")
+    if len(parts) > 1:
+        return parts[-1].lower()
+    return ""
+
+
+def get_game_mode(localized_game_mode):
+    parts = localized_game_mode.split(" ")
+    if len(parts) > 1:
+        return "_".join(parts[0:-1]).lower()
+    return localized_game_mode.lower()
+
+
+def get_team_code(region_no, game_mode, team_members):
+    team_members.sort(key=lambda team_member: team_member["id"])
+    result = f"{region_no}_{game_mode}"
+    for team_member in team_members:
+        result += f"_{team_member['id']}"
+    if game_mode == "1v1" and len(team_members) == 1 and "favoriteRace" in team_members[0]:
+        result += f"_{team_members[0]['favoriteRace'].lower()}"
+    return result
+
+
+def get_rate(value, total):
+    if total == 0:
+        return 0.00
+    else:
+        return round(value * 100 / total, 2)
+
+
+def get_valid_mmr(team):
+    if "mmr" in team:
+        if team["mmr"] > 2147483647:
+            return -1
+        return team["mmr"]
+    return 0
+
+
+def get_time(timestamp):
+    return datetime.fromtimestamp(timestamp)
+
+
+# 获取角色下所有天梯
+def get_character_all_ladders(region_no, realm_no, profile_no):
+    response = get_api_response(f"/sc2/profile/{region_no}/{realm_no}/{profile_no}/ladder/summary")
+    ladders = []
+    for membership in response["allLadderMemberships"]:
+        ladders.append(
+            {
+                "code": f"{region_no}_{membership['ladderId']}",
+                "number": int(membership["ladderId"]),
+                "regionNo": region_no,
+                "league": get_league(membership["localizedGameMode"]),
+                "gameMode": get_game_mode(membership["localizedGameMode"]),
+            }
+        )
+    return ladders
+
+
+# 获取指定天梯中所有队伍
+def get_ladder_all_teams(region_no, realm_no, profile_no, ladder):
+    response = get_api_response(f"/sc2/profile/{region_no}/{realm_no}/{profile_no}/ladder/{ladder['number']}")
+    teams = []
+    for team in response["ladderTeams"]:
+        teams.append(
+            {
+                "code": get_team_code(region_no, ladder["gameMode"], team["teamMembers"]),
+                "ladderCode": ladder["code"],
+                "points": team["points"],
+                "wins": team["wins"],
+                "losses": team["losses"],
+                "total": team["wins"] + team["losses"],
+                "winRate": get_rate(team["wins"], team["wins"] + team["losses"]),
+                "mmr": get_valid_mmr(team),
+                "joinLadderTime": get_time(team["joinTimestamp"]),
+                "teamMembers": team["teamMembers"],
+            }
+        )
+    return teams
--- main.py
+++ main.py
@@ -1,4 +1,122 @@
 from helpers import bnet_helper
+from helpers import redis_helper
+import pymongo
+from datetime import datetime
+import threading
+import traceback
+
+mongo_client = pymongo.MongoClient("mongodb://***REMOVED***:***REMOVED***@***REMOVED***:***REMOVED***")
+mongo_db = mongo_client.yf_sczone
+task_size = 10
+
+
+def update_ladder(ladder, character):
+    now = datetime.now()
+    ladder["updateTime"] = now
+    mongo_db.ladders.update_one(
+        {"code": ladder["code"]}, {"$set": ladder, "$setOnInsert": {"createTime": now}}, upsert=True
+    )
+    # TODO: api 更新 ladder
+
+    ladder_all_teams = bnet_helper.get_ladder_all_teams(
+        character["regionNo"], character["realmNo"], character["profileNo"], ladder
+    )
+    if len(ladder_all_teams) == 0:
+        return False
+    for ladder_team in ladder_all_teams:
+        now = datetime.now()
+        ladder_team["updateTime"] = now
+        mongo_db.teams.update_one(
+            {"code": ladder_team["code"]}, {"$set": ladder_team, "$setOnInsert": {"createTime": now}}, upsert=True
+        )
+        # TODO: api 更新 team, "membersData": json.dumps(team["teamMembers"]),
+        # TODO: 更新 mmr redis
+        print(f"update ladder_team: {ladder_team['code']}")
+        for team_member in ladder_team["teamMembers"]:
+            now = datetime.now()
+            character_code = f"{team_member['region']}_{team_member['realm']}_{team_member['id']}"
+            mongo_db.characters.update_one(
+                {"code": character_code},
+                {
+                    "$set": {
+                        "code": character_code,
+                        "regionNo": team_member["region"],
+                        "realmNo": team_member["realm"],
+                        "profileNo": team_member["id"],
+                        "displayName": team_member["displayName"],
+                        "clanTag": team_member["clanTag"] if "clanTag" in team_member else None,
+                        "updateTime": now,
+                    },
+                    "$setOnInsert": {"createTime": now},
+                },
+                upsert=True,
+            )
+            # TODO: api 更新 Character
+
+            team_member_code = f"{ladder_team['code']}_{character_code}"
+            mongo_db.teamMembers.update_one(
+                {"code": team_member_code},
+                {
+                    "$set": {
+                        "code": team_member_code,
+                        "teamCode": ladder_team["code"],
+                        "characterCode": character_code,
+                        "favoriteRace": team_member["favoriteRace"] if "favoriteRace" in team_member else None,
+                        "updateTime": now,
+                    },
+                    "$setOnInsert": {"createTime": now},
+                },
+                upsert=True,
+            )
+            # TODO: api 更新 TeamCharacter
+
+    return True
+
+
+def character_job():
+    for i in range(20):
+        threading.Thread(target=character_task).start()
+
+
+def character_task():
+    try:
+        task_index = redis_helper.redis_connection.incr("sczone-crawler:task:character:current")
+        skip = (task_index - 1) * task_size
+        characters = list(mongo_db.characters.find().sort("code").skip(skip).limit(task_size))
+        if len(characters) == 0:
+            # TODO, 执行完成
+            print("done")
+            redis_helper.redis_connection.delete("sczone-crawler:task:character:current")
+        for character in characters:
+            character_all_ladders = bnet_helper.get_character_all_ladders(
+                character["regionNo"], character["realmNo"], character["profileNo"]
+            )
+            for ladder in character_all_ladders:
+                ladder_active_redis_key = f"sczone-crawler:ladder:active:{ladder['code']}"
+                ladder_active = redis_helper.redis_connection.get(ladder_active_redis_key)
+                if ladder_active is None or ladder_active == "0":
+                    # TODO: api update ladder
+                    redis_helper.redis_connection.set(ladder_active_redis_key, "1")
+                    print(f"update_ladder: {ladder['code']}")
+                    update_ladder(ladder, character)
+                else:
+                    print(f"跳过更新 ladder: {ladder['code']}")
+        character_task()
+    except Exception:
+        print(traceback.format_exc())
+        threading.Timer(5, character_task).start()
+
 
 if __name__ == "__main__":
-    bnet_helper.get_access_token()
+    # print(bnet_helper.get_character_all_ladders(5, 1, 526043))
+    # update_ladder(
+    #     {"code": "5_63504", "number": 63504, "regionNo": 5, "league": "master", "gameMode": "1v1"},
+    #     {"regionNo": 5, "realmNo": 1, "profileNo": 526043},
+    # )
+    # character_job()
+    mongo_db.characters.create_index([("code", pymongo.ASCENDING)], name="idx_code", unique=True, background=True)
+    mongo_db.teams.create_index([("code", pymongo.ASCENDING)], name="idx_code", unique=True, background=True)
+    mongo_db.ladders.create_index([("code", pymongo.ASCENDING)], name="idx_code", unique=True, background=True)
+    mongo_db.teamMembers.create_index([("code", pymongo.ASCENDING)], name="idx_code", unique=True, background=True)
+
+    character_task()
