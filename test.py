214681286befc7057266545c9cdb8ee22c4870be
Bean <bean@yuanfen.net>
2021-10-21 20:08:44 +0800

    测试代码

--- test.py
+++ test.py
@@ -1,14 +1,22 @@
-from utils import api, json, redis, mongo
+from utils import api, json, redis, mongo, battlenet
 from pymongo import UpdateOne
 
 # data = api.get_api_response("/ladder/1")
 # print(json.dumps(data))
-test = redis.getset("test:x", 1)
-print(test)
-operations = [
-    UpdateOne({"code": 1}, {"$set": {"gg": 2}}, upsert=True),
-    UpdateOne({"code": 2}, {"$set": {"gg": 3}}, upsert=True),
-    UpdateOne({"code": 3}, {"$set": {"gg": 4}}, upsert=True),
-    UpdateOne({"code": 4}, {"$set": {"gg": 5}}, upsert=True),
-]
-mongo.mongo.test.bulk_write(operations)
+# test = redis.getset("test:x", 1)
+# print(test)
+# operations = [
+#     UpdateOne({"code": 1}, {"$set": {"gg": 2}}, upsert=True),
+#     UpdateOne({"code": 2}, {"$set": {"gg": 3}}, upsert=True),
+#     UpdateOne({"code": 3}, {"$set": {"gg": 4}}, upsert=True),
+#     UpdateOne({"code": 4}, {"$set": {"gg": 5}}, upsert=True),
+# ]
+# mongo.mongo.test.bulk_write(operations)
+
+teams = mongo.mongo.teams.find().limit(1000000)
+for team in teams:
+    if "regionNo" not in team or team["code"] != battlenet.get_team_code(
+        team["regionNo"], team["gameMode"], team["teamMembers"]
+    ):
+        mongo.mongo.teams.delete_one({"code": team["code"]})
+        print(team["code"])
