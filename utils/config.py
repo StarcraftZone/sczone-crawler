f00d32303d6fc2db4c6fb2458871b5bbda69f0b2
Bean <bean@yuanfen.net>
2021-10-29 11:11:43 +0800

    move mongo config to config.ini

--- utils/config.py
+++ utils/config.py
@@ -3,9 +3,10 @@ from configparser import ConfigParser
 config = ConfigParser()
 config.read("config.ini", "UTF-8")
 
-credentials = config["credentials"]
-redis = config["redis"]
 app = config["app"]
+credentials = config["credentials"]
+mongo = config["mongo"]
+redis = config["redis"]
 
 
 def getint(section, option):
--- utils/mongo.py
+++ utils/mongo.py
@@ -1,4 +1,7 @@
 import pymongo
+from utils import config
 
-mongo_client = pymongo.MongoClient("mongodb://***REMOVED***:***REMOVED***@***REMOVED***:***REMOVED***")
+mongo_client = pymongo.MongoClient(
+    f"mongodb://{config.mongo['user']}:{config.mongo['password']}@{config.mongo['host']}:{config.mongo['port']}"
+)
 mongo = mongo_client.yf_sczone
